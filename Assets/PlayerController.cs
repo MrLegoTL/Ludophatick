ace7d913ba6a620b410967da5cf4997b95f0700f
MrLegoTL <martingarciacurto@gmail.com>
2024-02-01 01:58:52 +0100

    inicio

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,174 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    #region VARIABLES
+    //muestra u oculta los gizmos del player
+    public bool showGizmos = false;
+
+    [Header("Player Movement")]
+    public float movementSpeed = 8f;
+    public float rotationSpeed = 14f;
+    public float acceleration = 30f;
+    private Vector3 direction;
+    private Vector3 desiredVelocity;
+    private float horizontal = 0f;
+    private float vertical = 0f;
+    private bool isMoving = false;
+
+    [Header("Jump")]
+    public float jumpForce = 10f;
+    public bool canJump = false;
+
+    [Header("Physics")]
+    public Rigidbody rb;
+    //layers considerados ground
+    public LayerMask groundLayer;
+    //transform para la posicion de chekeo de ground
+    public Transform groundCheck;
+    //booleana para especificar si estamos grounded
+    private bool grounded;
+    //tamaño del groundcheck
+    public Vector3 groundCheckSize;
+    
+
+    [Header("Animator")]
+    public Animator anim;
+
+    //solo vamos a comprobar si es mayor que 0, asin que no necesitamos mas capacidad
+    Collider[] colliderBuffer = new Collider[1];
+
+    #endregion
+    #region EVENTS
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GrounCheck();
+        Controls();
+        Movement();
+        AnimatorFeed();
+    }
+
+    private void OnDrawGizmos()
+    {
+        //si esta desactivado el mostrado de gizmo, slaimos del metodo sin hacer nada
+        if (!showGizmos) return;
+
+        Gizmos.color = Color.red;
+        //pintamos un cubo para visualizar el area de groundcheck
+        Gizmos.DrawWireCube(groundCheck.position, groundCheckSize);
+    }
+    #endregion
+    #region METHODS
+
+    /// <summary>
+    /// Rcuperamos la informacion de los Inputs
+    /// </summary>
+    private void Controls()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    /// <summary>
+    /// Realiza el desplazamiento del player
+    /// </summary>
+    private void Movement()
+    {
+        //componemos el vector de direccion deseado a partir del input
+        direction.Set(horizontal,0f,vertical);
+        //para asegurarnos que las diagonales no tienen una maagnitud superior a 1, "Clampeamos" su valor
+        direction = Vector3.ClampMagnitude(direction, 1f);
+
+        //calculamos la velocidad deseada en base a la driecion y la velocidad maxima
+        desiredVelocity = direction * movementSpeed;
+
+        if((horizontal!=0 || vertical != 0) && grounded)
+        {
+            //aplicamos la velocidad deseada , aumentando frame a frame en base a la aceleracion
+            rb.velocity = Vector3.MoveTowards(rb.velocity, desiredVelocity, Time.deltaTime * acceleration);
+
+            //debug ray para ver la direccion de velocidad del rigidbody
+            if (showGizmos) Debug.DrawRay(transform.position, rb.velocity);
+
+            //rotamos el tanque para que mire hacia la direccion que apunta la velocidad deseada
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+                                                   Quaternion.LookRotation(desiredVelocity),
+                                                   Time.deltaTime * rotationSpeed);
+
+            isMoving = true;
+        }
+        //En caso de no existir input, paramos la rotacion del tanque
+        if ((horizontal == 0 && vertical == 0))
+        {
+            rb.angularVelocity = Vector3.zero;
+            isMoving = false;
+        }
+    }
+
+    /// <summary>
+    /// Alimenta la informacion para el animator
+    /// </summary>
+    private void AnimatorFeed()
+    {
+        anim.SetBool("IsMoving", isMoving);
+        anim.SetBool("Grounded", grounded);
+        //// Activa la animación si el jugador se está moviendo
+        //if ((horizontal != 0 || vertical != 0))
+        //{
+        //    anim.SetBool("IsMoving", isMoving);
+        //}
+        //else if((horizontal == 0 && vertical == 0))
+        //{
+        //    anim.SetBool("IsMoving", !isMoving);
+        //}
+    }
+
+    /// <summary>
+    /// Comprueba si hay contacto con el suelo
+    /// </summary>
+    private void GrounCheck()
+    {
+        colliderBuffer[0] = null;
+        //comprobamos si hay contacto con el suelo mediante un overlap non alloc, para consumir menos recursos
+        //ya que esta comprobacion se hara de forma continua.
+        Physics.OverlapBoxNonAlloc(groundCheck.position,
+                                    groundCheckSize / 2,
+                                    colliderBuffer,
+                                    transform.rotation,
+                                    groundLayer);
+
+        //si el cvalor delprimer elemento del buffer es distinto de null, consideramos que estamos tocando el suelo
+        grounded = colliderBuffer[0] != null;
+    }
+
+    /// <summary>
+    /// Aplica una fuerza vertical para saltar
+    /// </summary>
+    private void Jump()
+    {
+        if (grounded)
+        {
+            
+            //si estamos encontacto con elsuelo, aplicamos una fuerza vertical de tipo impulso
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+        
+        
+    }
+    #endregion
+}
